d09429dfdea22126de70cbc51a280d0d25195a24
ForestLamp <y10b@ya.ru>
2022-05-16 16:36:14 +0300

    Create TabBarController first screen

---
+++ PhotoGalleryFromUnsplash/MainTabBarController.swift
@@ -0,0 +1,18 @@
+//
+//  MainTabBarController.swift
+//  PhotoGalleryFromUnsplash
+//
+//  Created by Alex Ch. on 16.05.2022.
+//
+
+import UIKit
+
+class MainTabBarController: UITabBarController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        view.backgroundColor = .gray
+    }
+    
+}
